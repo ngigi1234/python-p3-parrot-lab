7c813d019d13929e411ec105437d20dc88b8c525
ngigi1234 <francis.ngigi1@student.moringaschool.com>
2024-03-24 17:45:28 +0300

    Done

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,10 @@
-def parrot():
-    pass
+def parrot(argument = None):
+    if argument == None:
+        print("Squawk!")
+        return("Squawk!")
+    else:
+        print(argument)
+        return(argument)
+
+    
+    
